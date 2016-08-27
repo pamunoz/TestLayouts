7ea06fda8a5eb9bc5ff4d137d519ffe814b38efb
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-27 20:08:40 -0300

    Add a toast and other elements for testing

--- app/src/main/java/com/pfariasmunoz/testlayouts/TestLayoutsActivity.java
+++ app/src/main/java/com/pfariasmunoz/testlayouts/TestLayoutsActivity.java
@@ -1,7 +1,9 @@
 package com.pfariasmunoz.testlayouts;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TestLayoutsActivity extends Activity {
 
@@ -9,5 +11,22 @@ public class TestLayoutsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_layouts);
+
+        Context context = getApplicationContext();
+
+        CharSequence text = "Hello, I'm am a Toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
+
+
+
+
+
+
+
+
+
 }
