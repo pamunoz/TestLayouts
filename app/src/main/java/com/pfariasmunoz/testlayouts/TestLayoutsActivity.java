8cb3259709d5eb68372412f629ac42b0146cd805
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-27 18:27:56 -0300

    Initial commit (using LinearLayout)

---
+++ app/src/main/java/com/pfariasmunoz/testlayouts/TestLayoutsActivity.java
@@ -0,0 +1,13 @@
+package com.pfariasmunoz.testlayouts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestLayoutsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_layouts);
+    }
+}
